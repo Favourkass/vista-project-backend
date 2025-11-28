23df97f6346e9326ed877f27c302663c70ce44ac
Admin <favour.nnabue@jourhney.com>
2025-11-28 09:16:29 +0100

    url passing

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path, include
+from django.http import JsonResponse
+
+def api_root(request):
+    return JsonResponse({
+        'message': 'Lagos LGA API',
+        'endpoints': {
+            'lga-scores': '/api/lga-scores/',
+            'calculate-rankings': '/api/calculate-rankings/'
+        }
+    })
 
 urlpatterns = [
+    path('api', api_root, name='api-root'),
     path('api/', include('api.urls')),
 ]
 
